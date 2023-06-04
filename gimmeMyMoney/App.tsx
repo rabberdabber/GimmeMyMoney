42f4680d0412703cf4c71f8727e0540fb2401778
bake <111726582+bakecog@users.noreply.github.com>
2023-06-04 18:05:16 +0900

    include providers

--- gimmeMyMoney/App.tsx
+++ gimmeMyMoney/App.tsx
@@ -5,14 +5,20 @@
  * @format
  */
 
-import React from 'react';
+import * as React from 'react';
 import Navigation from './src/navigation/Navigation';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {PaperProvider, Portal} from 'react-native-paper';
+import PortalProvider from './src/context/portal/PortalProvider';
 
 function App(): JSX.Element {
   return (
     <SafeAreaProvider>
-      <Navigation />
+      <PaperProvider>
+        <PortalProvider>
+          <Navigation />
+        </PortalProvider>
+      </PaperProvider>
     </SafeAreaProvider>
   );
 }
